b14f5c58ffe9c13129a81a05ea0fdb1cb6fcd8f4
Vivi <varadivivien98@gmail.com>
2021-05-31 21:00:01 +0200

    finish textfield validation for email and password

--- ios-learning-app/extensions/TextFieldExtension.swift
+++ ios-learning-app/extensions/TextFieldExtension.swift
@@ -10,11 +10,16 @@ import Foundation
 import UIKit
 
 extension UITextField {
-    func isPasswordValid() -> Bool {
+    func isContentValid() -> Bool {
         if (self.textContentType == UITextContentType.password && self.text != nil) {
             if (self.text!.count >= 6) {
                 return true
             }
+        }
+        if (self.textContentType == UITextContentType.emailAddress && self.text != nil) {
+            let emailPattern = #"^\S+@\S+\.\S+$"#
+            let result = self.text?.range(of: emailPattern, options: .regularExpression)
+            return result != nil
         }
         return false
     }
