13347a6632987fcbe6e52c3ce336ace3ab2ee782
Vivi <varadivivien98@gmail.com>
2021-07-14 16:40:31 +0200

    created dashboard header cell files, commit before project upgrade

---
+++ ios-learning-app/View/TableView/Dashboard/Cells/DashboardHeaderCell/DashboardHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardHeaderCell.swift
+//  ios-learning-app
+//
+//  Created by New Mobile App on 2021. 07. 14..
+//  Copyright © 2021. Vivi Learns Everything. All rights reserved.
+//
+
+import UIKit
+
+class DashboardHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
