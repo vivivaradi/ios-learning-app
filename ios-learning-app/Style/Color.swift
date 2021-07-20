99709bc64890bc1cd0b4ebdf152cb430c6746842
Vivi <varadivivien98@gmail.com>
2021-07-20 12:03:38 +0200

    added color struct

---
+++ ios-learning-app/Style/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  ios-learning-app
+//
+//  Created by New Mobile App on 2021. 07. 20..
+//  Copyright © 2021. Vivi Learns Everything. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    let vodafoneRed = UIColor(red: 0.902, green: 0, blue: 0, alpha: 1)
+    let lightGrey = UIColor(red: 0.686, green: 0.686, blue: 0.686, alpha: 1)
+    let mediumGrey = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+    let darkGrey = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    let white = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    let shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25)
+    let borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+}
