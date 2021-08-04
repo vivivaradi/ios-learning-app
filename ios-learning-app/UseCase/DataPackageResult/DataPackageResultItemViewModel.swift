2c60b06aea20744151aa883a3e61fdb0262f18b0
Vivi <varadivivien98@gmail.com>
2021-08-04 15:48:58 +0200

    created itemviewmodel, slight modification in mock files

---
+++ ios-learning-app/UseCase/DataPackageResult/DataPackageResultItemViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataPackageResultItemViewModel.swift
+//  ios-learning-app
+//
+//  Created by New Mobile App on 2021. 08. 04..
+//  Copyright © 2021. Vivi Learns Everything. All rights reserved.
+//
+
+import Foundation
+
+struct DataPackageResultItemViewModel {
+    let status: String
+    let title: String
+    let description: String
+}
